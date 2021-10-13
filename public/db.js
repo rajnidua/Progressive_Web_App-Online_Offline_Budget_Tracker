b1e949f3fc651d0b772edcaf2084b3098e35835e
Rajni Dua <rajni.dua14@gmail.com>
2021-10-13 23:30:49 +1030

    object store created

--- public/db.js
+++ public/db.js
@@ -1,5 +1,14 @@
+let db;
+
 // We request a database instance.
 const request = indexedDB.open("budgetDatabase", 1);
+
+// Create an object store inside the onupgradeneeded method.
+request.onupgradeneeded = ({ target }) => {
+  const db = target.result;
+  const objectStore = db.createObjectStore("budgetDatabase");
+  console.log(objectStore);
+};
 
 // This returns a result that we can then manipulate.
 request.onsuccess = (event) => {
